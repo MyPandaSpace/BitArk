e4a9b527d6f467b9cba29ad8932ae775c2953ccd
Panda <panddabboy@gmail.com>
2025-01-14 00:10:42 +0800

    Day8_update(0114)

--- BitArk/src/Components/Navbar.jsx
+++ BitArk/src/Components/Navbar.jsx
@@ -5,6 +5,12 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 	const [isScrolled, setIsScrolled] = useState(false);
 
+	const [isMenuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!isMenuOpen);
+	};
+
 	// 滾動事件處理函數
 	const handleScroll = () => {
 		if (window.scrollY > 100) {
@@ -29,7 +35,12 @@ const Navbar = () => {
 			<div className="navbar-logo">
 				<Link to="/" className="logo"><img src="./icons/logo.svg" alt="" /><span>幣方舟</span></Link>
 			</div>
-			<ul className="navbar-links">
+			<div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			<ul className={`navbar-links ${isMenuOpen ? 'show' : ''}`}>
 				<li><Link to="/NEWS">NEWS</Link></li>
 				<li><Link to="/Crypto">貨幣介紹</Link></li>
 				<li><Link to="/FAQ">常見問題</Link></li>
